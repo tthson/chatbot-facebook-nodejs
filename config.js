e3c42e8faac5bc0c033b8dc84ba3c95ce3c6404a
tranthaison <tthsonvn@gmail.com>
2017-05-01 15:09:04 +0700

    config for master

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAGOp7RZA8zMBAC4tzA2FeZAcneMa84Mj42G22HVoh18XDv17PtgjzZCsGCbcG7d66OncoC2qYPaSrcorZAtTfFNVerpxjfA5RKKe6ckZBCiHhgyXZBTuFUpO2xuxn8Nc5ZAH8b3qCgYeCBz6p9uO8Qw9tPrSYkuTwKUcqmuQHOtQZDZD',
-    FB_VERIFY_TOKEN: 'somethingisfirstwithson',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
+    FB_VERIFY_TOKEN: 'something_is_first_with_son',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ed2e9df524aa415d86d7439fe2bab7d7',
+    FB_APP_SECRET: '381bf6d4d4ff5ce8856559f9b21852f2',
     SERVER_URL: "https://evening-tundra-35383.herokuapp.com/",
 };
