bfe2d341d9d32bd700f337dacee0e3d3affee900
tranthaison <tthsonvn@gmail.com>
2017-05-01 14:59:22 +0700

    config for master

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAGOp7RZA8zMBAC4tzA2FeZAcneMa84Mj42G22HVoh18XDv17PtgjzZCsGCbcG7d66OncoC2qYPaSrcorZAtTfFNVerpxjfA5RKKe6ckZBCiHhgyXZBTuFUpO2xuxn8Nc5ZAH8b3qCgYeCBz6p9uO8Qw9tPrSYkuTwKUcqmuQHOtQZDZD',
+    FB_VERIFY_TOKEN: 'somethingisfirstwithson',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://bots.api.ai/facebook/d97351db-15c0-4fea-9637-c45dad805f55/webhook/",
 };
